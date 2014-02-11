a60022dcff1886d086e1c01d8148ff58f3a397ef
Stefan Eng <stefaneng13@gmail.com>
2014-02-10 22:29:55 -0800

    Initial hello world commit, with slightly updated travis.yml

---
+++ src/com/powerout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.powerout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.power.out.R;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
