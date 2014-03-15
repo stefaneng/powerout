35005d12cf0a273d54a0839465adc613eb9b352b
Stefan Eng <stefaneng13@gmail.com>
2014-03-15 12:38:13 -0700

    Successfully queries Contacts with CursorLoader

--- src/com/powerout/MainActivity.java
+++ src/com/powerout/MainActivity.java
@@ -18,14 +18,19 @@
 package com.powerout;
 
 import android.app.Activity;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 import com.power.out.R;
 
 public class MainActivity extends Activity {
+    static final String TAG = "com.powerout.MainActivity";
     static final int PICK_CONTACT_REQUEST = 1;
 
     @Override
@@ -38,6 +43,37 @@ public class MainActivity extends Activity {
         Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
         pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
         startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Get the URI that points to the selected contact
+                Uri contactUri = data.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                };
+
+                CursorLoader cursorLoader = new CursorLoader(getApplicationContext(),
+                        contactUri, projection, null, null, null);
+
+                Cursor cursor = cursorLoader.loadInBackground();
+
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+
+                Log.i(TAG, "Phone number: " + number.toString());
+                // Do something with the phone number...
+            }
+        }
     }
 
 }
